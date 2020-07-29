07cd0fcd63facc858d95c2b3142d9499a9cd162b
Haitao Lu <lht19900714@gmail.com>
2020-07-28 20:19:17 -0400

    add 621 and 34 solution

--- Algorithms/Python/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Find_First_and_Last_Position_of_Element_in_Sorted_Array_Solution_1.py
+++ Algorithms/Python/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Find_First_and_Last_Position_of_Element_in_Sorted_Array_Solution_1.py
@@ -4,6 +4,7 @@
 
 class Solution:
     def searchRange(self, nums, target):
+
         def find_first(nums, target):
             left = 0
             right = len(nums) - 1
@@ -14,10 +15,9 @@ class Solution:
                     right = mid
                 else:
                     left = mid + 1
-            if nums[left] == target:
-                return left
-            if nums[right] == target:
-                return right
+
+            if nums[left] == target: return left
+            if nums[right] == target: return right
             return -1
 
         def find_last(nums, target):
@@ -30,15 +30,13 @@ class Solution:
                     left = mid
                 else:
                     right = mid - 1
-            if nums[right] == target:
-                return right
-            if nums[left] == target:
-                return left
+
+            if nums[right] == target: return right
+            if nums[left] == target: return left
             return -1
 
         if len(nums) == 0: return [-1, -1]
         return [find_first(nums, target), find_last(nums, target)]
-
 
 
 
