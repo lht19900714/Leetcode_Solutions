f303dbf46b619a7e98e4b23479a5484134a617ad
jhaitaolu <jhaitaolu@tradesun.com>
2021-06-30 12:30:43 -0400

    add 236 solution

---
+++ Algorithms/0236_Lowest_Common_Ancestor_of_a_Binary_Tree/Java/LowestCommonAncestorofaBinaryTreeSolution1.java
@@ -0,0 +1,33 @@
+
+// Space: O(1)
+// Time: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return dfs(root,p,q);
+    }
+
+    private TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+
+        TreeNode left = dfs(root.left, p, q);
+        TreeNode right = dfs(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        if (right != null) return right;
+        return null;
+    }
+}
+
+
+
+
