266fea0afadf283c5708d11ed2e2da7648142354
jhaitaolu <jhaitaolu@tradesun.com>
2021-07-26 15:00:10 -0400

    add 108 solution

---
+++ Algorithms/0108_Convert_Sorted_Array_to_Binary_Search_Tree/Java/ConvertSortedArraytoBinarySearchTreeSolution1.java
@@ -0,0 +1,40 @@
+
+// Space: O(1)
+// Time: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        if (nums.length == 1) return new TreeNode(nums[0]);
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int left, int right) {
+        if (right < left) return null;
+        if (left == right) return new TreeNode(nums[left]);
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, left, mid - 1);
+        root.right = dfs(nums, mid + 1, right);
+        return root;
+    }
+}
+
+
+
+
