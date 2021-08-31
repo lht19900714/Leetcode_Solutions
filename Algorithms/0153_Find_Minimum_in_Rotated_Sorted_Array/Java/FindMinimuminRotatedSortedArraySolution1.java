bba5cf95850ed7446bcd83545036395c92e311a4
Haitao Lu <lht19900714@gmail.com>
2021-09-01 00:36:39 +0800

    add 153 solution

---
+++ Algorithms/0153_Find_Minimum_in_Rotated_Sorted_Array/Java/FindMinimuminRotatedSortedArraySolution1.java
@@ -0,0 +1,24 @@
+
+// Space: O(1)
+// Time: O(logn)
+
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.min(nums[0], nums[1]);
+        int left = 0, right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            }
+        }
+        return Math.min(nums[left], nums[right]);
+    }
+}
+
+
+
+
